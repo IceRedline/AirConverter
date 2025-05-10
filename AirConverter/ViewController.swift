de805dc0d9df22d97a270f23157125ec459300ed
IceRedline <scpd2604@gmail.com>
2025-05-10 18:58:14 +0300

    Initial Commit

---
+++ AirConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirConverter
+//
+//  Created by Артем Табенский on 10.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
